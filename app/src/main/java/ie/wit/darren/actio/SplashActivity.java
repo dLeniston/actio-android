49124410f750db8b7f39a28aecc9f71c728763de
Darren Leniston <dleniston@yahoo.com>
2018-02-25 15:42:58 +0000

    Added readme.md file

---
+++ app/src/main/java/ie/wit/darren/actio/SplashActivity.java
@@ -0,0 +1,30 @@
+package ie.wit.darren.actio;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Dazza on 25/02/2018.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+
+    }
+}
---
+++ app/src/readme.md
@@ -0,0 +1,3 @@
+### Actio Android Application ###
+
+This is the accompanying android application for my 4th year project for Waterford Institute of Technology.
