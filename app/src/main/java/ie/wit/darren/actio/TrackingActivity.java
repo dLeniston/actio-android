cd911eafe8d22c44f08e65de92fa35e543c94e3d
Darren Leniston <dleniston@yahoo.com>
2018-02-25 18:06:46 +0000

    Added spinner options for tracking menu and resources file

--- app/src/main/java/ie/wit/darren/actio/TrackingActivity.java
+++ app/src/main/java/ie/wit/darren/actio/TrackingActivity.java
@@ -2,15 +2,28 @@ package ie.wit.darren.actio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /**
  * Created by Dazza on 25/02/2018.
  */
 
 public class TrackingActivity extends AppCompatActivity{
+    Spinner trackingMenuSpinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tracking_screen);
+
+        trackingMenuSpinner = (Spinner) findViewById(R.id.trackingMenuSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.tracking_options, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        trackingMenuSpinner.setAdapter(adapter);
     }
 }
